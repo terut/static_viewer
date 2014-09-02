28e32d748c2ece3deccccad7f2173cd1829a795b
terut <terut.dev+github@gmail.com>
2014-09-03 02:05:00 +0900

    Add basic auth

--- server.rb
+++ server.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 
 module StaticViewer
   class Server < Sinatra::Base
+    use Rack::Auth::Basic, "Restricted Area" do |username, password|
+      username == ENV['AUTH_USENAME'] and password == ENV['AUTH_PASSWORD']
+    end
+
     get '/' do
       'Hello World'
     end
