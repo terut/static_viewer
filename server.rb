96537f20027ac4d8a7eb006046769fd548525c67
terut <terut.dev+github@gmail.com>
2014-09-05 01:42:22 +0900

    Add vue.js

--- server.rb
+++ server.rb
@@ -1,13 +1,31 @@
 require 'sinatra'
+require 'json'
 
 module StaticViewer
   class Server < Sinatra::Base
-    use Rack::Auth::Basic, "Restricted Area" do |username, password|
-      username == ENV['AUTH_USENAME'] and password == ENV['AUTH_PASSWORD']
-    end
+    #use Rack::Auth::Basic, "Restricted Area" do |username, password|
+    #  username == ENV['AUTH_USENAME'] and password == ENV['AUTH_PASSWORD']
+    #end
 
     get '/' do
-      'Hello World'
+      @tree = JSON.dump(tree('public/test'))
+      slim :index
+    end
+
+    def tree(path)
+      data = []
+      Dir.foreach(path) do |entry|
+        next if (entry == '..' || entry == '.')
+
+        child = {}
+        full_path = File.join(path, entry)
+        if File.directory?(full_path)
+          child[:children] = tree(full_path)
+        end
+        child[:name] = entry
+        data << child
+      end
+      data
     end
   end
 end
